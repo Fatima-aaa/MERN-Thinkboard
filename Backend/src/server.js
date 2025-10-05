1d3fd21afcc6a52a3f1e28d106119ade49e3ff5b
Fatima <fatimatahir440@gmail.com>
2025-10-06 02:32:28 +0500

    made the controllers functional plus added middleware

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,16 +2,20 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5006;
 
-connectDB();
 
+app.use(express.json());
+app.use(rateLimiter)
 app.use("/api/notes", notesRoutes);
 
+connectDB().then(() => {
 app.listen(PORT, () => {
     console.log('Server is running on port' , PORT);
 });
+})
